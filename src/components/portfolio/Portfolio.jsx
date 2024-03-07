6d95af3a2b1d27096dc3a27e87d78fbb65a73778
vijay-dev-x <vm2218943@gmail.com>
2024-03-08 00:18:36 +0530

    added link

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,25 +5,29 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "Panda VPN clone",
+    title: "Panda VPN UI clone",
     img: "/portfolio1.webp",
+    link: "https://panda-vpn.netlify.app/",
     desc: "The creation of a Panda VPN clone using React involved meticulous design and development efforts, ensuring seamless functionality and user experience. Implementing mobile responsiveness was a crucial aspect, guaranteeing optimal performance across devices. Through innovative design choices and diligent coding practices, the project delivers a reliable and user-friendly VPN solution.",
   },
   {
     id: 2,
     title: "Social Site",
+    link: "https://social-clone551.netlify.app/",
     img: "https://codecanyon.img.customer.envatousercontent.com/files/387482830/inline.jpeg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=300&s=438a989f36123431f62793af286330fc",
-    desc: "Developing a social site clone with React required comprehensive planning and execution, focusing on replicating key features and user interactions of popular social platforms. Incorporating mobile responsiveness was essential, ensuring accessibility across devices. Meticulous attention to detail in design and functionality resulted in a seamless and engaging user experience reminiscent of leading social networks.",
+    desc: "Developing a social site clone with React required comprehensive planning and execution, focusing on replicating key features and user interactions of popular social platforms. Incorporating mobile responsiveness was essential, ensuring accessibility across devices. Meticulous attention to detail in design and functionality resulted in a seamless and engaging user experience reminiscent of leading social networks.(!! NOT MOBILE RESPOSIVE)",
   },
   {
     id: 3,
     title: "Interactive 3D glass weather",
+    link: "https://weather551.netlify.app/",
     img: "https://shop.loxone.com/media/catalog/product/cache/10/image/600x400/9df78eab33525d08d6e5fb8d27136e95/1/0/100026_weather-services_1.png",
     desc: "Creating a social site clone with React involved meticulous design and development, mirroring essential features and user interactions of prominent social platforms. Emphasis was placed on mobile responsiveness, guaranteeing accessibility across devices. Through comprehensive planning and attention to detail in design and functionality, the project delivers a captivating and intuitive user experience akin to leading social networks.",
   },
   {
     id: 4,
     title: "Netflix Clone",
+    link: "https://netflix551-clone.netlify.app/",
     img: "https://i.ytimg.com/vi/Tgat3-prVv4/maxresdefault.jpg",
     desc: "Crafting a Netflix clone with HTML and CSS necessitated intricate design and implementation, replicating the iconic layout and functionalities of the streaming giant. Prioritizing responsiveness, the site seamlessly adapts to various screen sizes. Through meticulous styling and structure, the project delivers an immersive user experience reminiscent of the original platform.",
   },
@@ -48,7 +52,9 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <button>
+              <a href={item.link}>Open Project</a>
+            </button>
           </motion.div>
         </div>
       </div>
